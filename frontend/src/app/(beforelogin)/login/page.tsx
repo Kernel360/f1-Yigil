7cd5728d76788516122ca8efc3651e18de3299d3
davidktlee <rudxor567@naver.com>
2023-12-13 13:10:07 +0900

    feat: 랜딩페이지 생성

--- frontend/src/app/(beforelogin)/login/page.tsx
+++ frontend/src/app/(beforelogin)/login/page.tsx
@@ -6,8 +6,8 @@ import mainLogo from '/public/2gil-logo-main.png'
 import { useSession } from 'next-auth/react'
 
 import { redirect } from 'next/navigation'
-import KakaoBtn from '@/app/_components/ui/button/Kakao'
-import GoogleBtn from '@/app/_components/ui/button/Google'
+// import KakaoBtn from '@/app/_components/ui/button/Kakao'
+// import GoogleBtn from '@/app/_components/ui/button/Google'
 
 export default function LoginPage() {
   const { data: session } = useSession()
@@ -17,12 +17,12 @@ export default function LoginPage() {
   }, [])
   return (
     <div className="w-full h-screen bg-main flex flex-col justify-center items-center">
-      <Image src={mainLogo} alt="logo" width={400} />
+      {/* <Image src={mainLogo} alt="logo" width={400} />
       <div className="text-2xl text-[white] font-bold">서비스를 사용하시려면 로그인 해주세요</div>
       <div className="mt-8 flex flex-col items-center justify-center gap-4">
         <KakaoBtn />
         <GoogleBtn />
-      </div>
+      </div> */}
     </div>
   )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from 'next/image'
+'use client'
+
+import { useEffect } from 'react'
+import LandingPage from './_components/landing/LandingPage'
+
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  return <div>햐이</div>
+  const router = useRouter()
+  // 세션 확인 후 로그인으로 보낼지 메인페이지로 보낼지.
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/login')
+    }, 1500)
+    return () => clearTimeout(timer)
+  }, [])
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
 }
