28b7d38185445aa1e86ff493568f84d86d29a98a
davidktlee <rudxor567@naver.com>
2023-12-13 12:56:14 +0900

    design: 폰트 설정 및 아이콘 추가

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundImage: {},
+    },
+    colors: {
+      main: '#FC5230',
     },
   },
+
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }
-export default config
