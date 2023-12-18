c280926735bee9eb12b761b096b99d9d6a3bc860
Jeongwook Park <hatchling3713@gmail.com>
2023-12-18 21:42:00 +0900

    conf: test 파일 수정

--- backend/src/test/java/kr/co/yigil/login/application/strategy/KakaoLoginStrategyTest.java
+++ backend/src/test/java/kr/co/yigil/login/application/strategy/KakaoLoginStrategyTest.java
@@ -32,7 +32,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(KakaoLoginStrategy.class)
+@SpringBootTest(classes = KakaoLoginStrategy.class)
 public class KakaoLoginStrategyTest {
 
     @MockBean
