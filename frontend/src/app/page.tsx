437c437dacb6cf48ed3aeb430db96adb91d94515
Jeongwook Park <hatchling3713@gmail.com>
2023-12-18 19:18:19 +0900

    chore: 코드 포매터 적용

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+'use client';
 
-import { useEffect } from 'react'
-import LandingPage from './_components/landing/LandingPage'
+import { useEffect } from 'react';
+import LandingPage from './_components/landing/LandingPage';
 
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   // 세션 확인 후 로그인으로 보낼지 메인페이지로 보낼지.
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/login')
-    }, 1500)
-    return () => clearTimeout(timer)
-  }, [])
-  return <LandingPage />
+      router.push('/login');
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+  return <LandingPage />;
 }
