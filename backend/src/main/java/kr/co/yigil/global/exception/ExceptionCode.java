b4811ec049489a4545d1bfd2bdcfc869462eeb4a
StoneHee99 <kiit0901@gmail.com>
2023-12-19 15:21:03 +0900

    feat: add memberOnly annotation for identify member

--- backend/src/main/java/kr/co/yigil/global/exception/ExceptionCode.java
+++ backend/src/main/java/kr/co/yigil/global/exception/ExceptionCode.java
@@ -9,7 +9,8 @@ public enum ExceptionCode {
 
     INVALID_REQUEST(1000, "올바르지 않은 요청입니다."),
 
-    INVALID_ACCESS_TOKEN(9101, "올바르지 않은 형식의 Access Token입니다.");
+    INVALID_ACCESS_TOKEN(9101, "올바르지 않은 형식의 Access Token입니다."),
+    INVALID_AUTHORITY(9201, "해당 요청에 대한 접근 권한이 없습니다.");
 
     private final int code;
     private final String message;
