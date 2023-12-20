8db4d09f6275f9b606a32726fea1909fc6f6cd69
Jung Wook Park <hatchling3713@gmail.com>
2023-12-20 15:53:13 +0900

    feat: 구글 로그인 버튼과 NextAuth 기능 연결 fix: 카카오 로그인 버튼 callbackUrl 수정

--- frontend/src/app/_components/ui/button/Kakao.tsx
+++ frontend/src/app/_components/ui/button/Kakao.tsx
@@ -11,7 +11,7 @@ export default function KakaoBtn() {
       className="bg-[#fee500] w-[300px] h-[45px] flex justify-start text-center items-center rounded-md p-[15px] cursor-pointer border-none"
       onClick={() =>
         signIn('kakao', {
-          callbackUrl: '/home',
+          callbackUrl: '/',
         })
       }
     >
