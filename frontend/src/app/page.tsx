31dba4c1ac27f38770e0a10023348dc9483de0b3
Jung Wook Park <hatchling3713@gmail.com>
2023-12-22 17:56:15 +0900

    feat: 홈 페이지 하단에 내비게이션 바 추가 feat: 마크업 구조 작성 feat: 레이아웃 적용 feat: 링크 경로 설정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/app/_components/header/Header';
+import { NavigationBar } from './_components/navigation';
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <NavigationBar />
+    </main>
   );
 }
