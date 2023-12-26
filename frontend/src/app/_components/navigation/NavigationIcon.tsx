e1eae98f14725e8ad222858182bd4521f184afe8
Jung Wook Park <hatchling3713@gmail.com>
2023-12-26 18:05:10 +0900

    refactor: 임의로 나눈 Tailwind class를 한 줄로 작성

--- frontend/src/app/_components/navigation/NavigationIcon.tsx
+++ frontend/src/app/_components/navigation/NavigationIcon.tsx
@@ -13,19 +13,14 @@ export default function NavigationIcon({
   active?: boolean;
   Icon: ComponentType<{ className?: string }>;
 }) {
-  const iconStyles = active ? 'fill-main' : 'fill-[#000000]/[.4]';
-
-  const styles = [
-    'w-16',
-    'flex flex-col gap-2 justify-center items-center',
-    'text-xs text-[#000000]/[.4] no-underline',
-    'hover:text-main focus:text-main',
-    active ? 'text-main' : '',
-  ].join(' ');
+  const activeLinkStyle = active ? 'text-main' : '';
 
   return (
-    <Link href={href} className={styles}>
-      <Icon className={iconStyles} />
+    <Link
+      href={href}
+      className={`w-16 flex flex-col gap-2 justify-center items-center text-xs text-[#000000]/[.4] no-underline hover:text-main focus:text-main ${activeLinkStyle}`}
+    >
+      <Icon className={`${active ? 'fill-main' : 'fill-[#000000]/[.4]'}`} />
       {label}
     </Link>
   );
