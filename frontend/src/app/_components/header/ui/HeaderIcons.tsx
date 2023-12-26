5bc219337acc1516ccf93476513e6713ee357067
Jung Wook Park <hatchling3713@gmail.com>
2023-12-26 15:18:02 +0900

    refactor: svgr 도입에 따른 svg 처리 방식 변경, 코드 수정

--- frontend/src/app/_components/header/ui/HeaderIcons.tsx
+++ frontend/src/app/_components/header/ui/HeaderIcons.tsx
@@ -1,17 +1,16 @@
-import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+
 import SearchIcon from '/public/icons/search.svg';
 import BellIcon from '/public/icons/bell.svg';
-import Link from 'next/link';
 
 export default function HeaderIcons() {
   return (
-    <div className="absolute right-4 flex items-center gap-x-2 ">
+    <div className="absolute right-4 flex items-center gap-x-2">
       <Link href="/search">
-        <Image src={SearchIcon} alt="search-icon" />
+        <SearchIcon />
       </Link>
       <Link href="/notification">
-        <Image src={BellIcon} alt="bell-icon" />
+        <BellIcon />
       </Link>
     </div>
   );
