89b2ae5564ad84712da0fbedd3a2f71e32b47f4d
Jung Wook Park <hatchling3713@gmail.com>
2023-12-26 21:18:55 +0900

    refactor: url에 따른 링크 active 처리를 Next hook을 사용하도록 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Header from '@/app/_components/header/Header';
-import { NavigationBar } from './_components/navigation';
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <NavigationBar />
     </main>
   );
 }
