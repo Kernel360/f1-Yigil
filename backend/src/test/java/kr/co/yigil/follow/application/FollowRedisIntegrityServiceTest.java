7a36da7e1d08931fd3437d55ade32d283b0f4fae
StoneHee99 <kiit0901@gmail.com>
2023-12-28 12:21:39 +0900

    test: add test code for redisRepository

--- backend/src/test/java/kr/co/yigil/follow/application/FollowRedisIntegrityServiceTest.java
+++ backend/src/test/java/kr/co/yigil/follow/application/FollowRedisIntegrityServiceTest.java
@@ -1,13 +1,14 @@
 package kr.co.yigil.follow.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import kr.co.yigil.follow.domain.FollowCount;
+import kr.co.yigil.follow.domain.repository.FollowCountRepository;
 import kr.co.yigil.follow.domain.repository.FollowRepository;
 import kr.co.yigil.follow.dto.FollowCountDto;
 import kr.co.yigil.member.domain.Member;
@@ -15,56 +16,63 @@ import kr.co.yigil.member.domain.SocialLoginType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 public class FollowRedisIntegrityServiceTest {
-
-    @Mock
-    private RedisTemplate<String, Object> redisTemplate;
 
     @Mock
     private FollowRepository followRepository;
 
     @Mock
-    private ValueOperations<String, Object> valueOperations;
+    private FollowCountRepository followCountRepository;
 
+    @InjectMocks
     private FollowRedisIntegrityService followRedisIntegrityService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
-        followRedisIntegrityService = new FollowRedisIntegrityService(redisTemplate, followRepository);
     }
 
-    @DisplayName("Redis에 FollowCount가 존재하지 않을 경우 DB에 조회 후 저장하는지")
+    @DisplayName("ensureFollowCounts 메서드가 이미 존재하는 FollowCount를 반환하는지")
     @Test
-    void testEnsureFollowCountsWhenNotExist() {
+    void testEnsureFollowCountsWhenAlreadyExists() {
+        // Given
+        Member member = new Member(1L, "email", "12345678", "member", "image.jpg", SocialLoginType.KAKAO);
+        FollowCount existingFollowCount = new FollowCount(1L, 10, 5);
+
+        when(followCountRepository.findByMemberId(member.getId())).thenReturn(Optional.of(existingFollowCount));
+
+        // When
+        FollowCount result = followRedisIntegrityService.ensureFollowCounts(member);
+
+        // Then
+        assertThat(result).isEqualTo(existingFollowCount);
+        verify(followRepository, never()).getFollowCounts(any());
+        verify(followCountRepository, never()).save(any());
+    }
+
+    @DisplayName("ensureFollowCounts 메서드가 존재하지 않을 경우 FollowCount를 생성하고 저장하는지")
+    @Test
+    void testEnsureFollowCountsWhenNotExists() {
+        // Given
         Member member = new Member(1L, "email", "12345678", "member", "image.jpg", SocialLoginType.KAKAO);
         FollowCountDto followCountDto = new FollowCountDto(10L, 5L);
-        FollowCount followCount = new FollowCount(1L, 10, 5);
+        FollowCount newFollowCount = new FollowCount(1L, 10, 5);
 
-        when(redisTemplate.hasKey("followCount:1")).thenReturn(false);
+        when(followCountRepository.findByMemberId(member.getId())).thenReturn(Optional.empty());
         when(followRepository.getFollowCounts(member)).thenReturn(followCountDto);
+        when(followCountRepository.save(any())).thenReturn(newFollowCount);
 
-        followRedisIntegrityService.ensureFollowCounts(member);
+        // When
+        FollowCount result = followRedisIntegrityService.ensureFollowCounts(member);
 
-        verify(valueOperations).set(eq("followCount:1"), any(FollowCount.class));
-    }
-
-    @DisplayName("Redis에 FollowCount가 존재할 경우 DB 조회 및 저장 로직이 실행되지 않는지")
-    @Test
-    void testEnsureFollowCountsWhenExists() {
-        Member member = new Member(1L, "email", "12345678", "member", "image.jpg", SocialLoginType.KAKAO);
-
-        when(redisTemplate.hasKey("followCount:1")).thenReturn(true);
-
-        followRedisIntegrityService.ensureFollowCounts(member);
-
-        verify(valueOperations, never()).set(anyString(), any());
-        verify(followRepository, never()).getFollowCounts(any(Member.class));
+        // Then
+        assertThat(newFollowCount.getFollowerCount()).isEqualTo(result.getFollowerCount());
+        assertThat(newFollowCount.getFollowingCount()).isEqualTo(result.getFollowingCount());
+        verify(followRepository).getFollowCounts(member);
+        verify(followCountRepository).save((FollowCount) any());
     }
 }
