58689463e74701bd2d9c7d749811c3bccf732daf
davidktlee <rudxor567@naver.com>
2024-01-05 22:06:40 +0900

    refactor: Jest 테스트 라이브러리 vitest 라이브러리로 변경

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -32,6 +32,16 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
