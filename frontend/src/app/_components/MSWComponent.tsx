11fca2ed90d54d1f6e0e29d8b5887e6af47fff9e
davidktlee <rudxor567@naver.com>
2024-01-06 08:01:54 +0900

    fix: MSW 컴포넌트에서 조건문의 잘못된 조건 변경

--- frontend/src/app/_components/MSWComponent.tsx
+++ frontend/src/app/_components/MSWComponent.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 export default function MSWComponent() {
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'production') {
       return;
     } else if (typeof window !== 'undefined') {
       require('@/mocks/browser');
