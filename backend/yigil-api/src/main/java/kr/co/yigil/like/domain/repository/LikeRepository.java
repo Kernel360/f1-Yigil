b62ab3dcf79d71b665f0610aea22e5d8e2955cad
StoneHee99 <kiit0901@gmail.com>
2024-01-15 10:20:20 +0900

    merge with upstream

---
+++ backend/yigil-api/src/main/java/kr/co/yigil/like/domain/repository/LikeRepository.java
@@ -0,0 +1,14 @@
+package kr.co.yigil.like.domain.repository;
+
+import kr.co.yigil.like.domain.Like;
+import kr.co.yigil.member.domain.Member;
+import kr.co.yigil.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    public int countAllByPost(Post post);
+
+    public void deleteByMemberAndPost(Member member, Post post);
+
+}
