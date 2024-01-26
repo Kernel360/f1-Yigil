6237fbfe83065ac7a91584ac637bcc106c6f2edc
StoneHee99 <kiit0901@gmail.com>
2024-01-26 12:48:03 +0900

    add frontend page with shadcn/ui

---
+++ admin-frontend/yigil-admin/src/components/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./Layout";
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>환영합니다!</h1>
+      <p>이곳은 이길어때 어드민 페이지입니다.</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
