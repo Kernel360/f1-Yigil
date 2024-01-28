9ff2950b82ab98d67afe64e614dbf641ba9d8b45
davidktlee <rudxor567@naver.com>
2024-01-28 15:59:01 +0900

    refactor: tabIndex를 통해 키보드 탭을 이용한 접근성 추가

--- frontend/src/app/_components/mypage/MyPlace.tsx
+++ frontend/src/app/_components/mypage/MyPlace.tsx
@@ -28,6 +28,7 @@ export default function MyPageMyPlace() {
         {myPageMyPlaceTab.map((place, idx) => (
           <div
             key={idx}
+            tabIndex={0}
             className={`border-2 rounded-full px-4 py-2 text-xl leading-6 cursor-pointer ${
               tabState === idx
                 ? 'bg-gray-700 text-white border-white'
