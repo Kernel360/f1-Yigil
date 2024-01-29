647d861eed74b7d1019c70d06ec04bfdf0815576
davidktlee <rudxor567@naver.com>
2024-01-29 16:41:33 +0900

    style: 카카오 로그인 스타일 수정

--- frontend/src/app/_components/ui/button/Kakao.tsx
+++ frontend/src/app/_components/ui/button/Kakao.tsx
@@ -7,7 +7,7 @@ import KakaoLogo from '/public/loginBtns/kakao_logo.svg';
 export default function KakaoBtn() {
   return (
     <button
-      className="w-full flex justify-center gap-x-2 items-center rounded-full py-3 cursor-pointer border-none bg-[#fee500] hover:bg-[#e6cf00] active:bg-[#ccb800]"
+      className="w-full flex justify-center gap-x-2 items-center rounded-2xl py-3 cursor-pointer border-none bg-[#fee500] hover:bg-[#e6cf00] active:bg-[#ccb800]"
       onClick={() =>
         signIn('kakao', {
           callbackUrl: '/',
