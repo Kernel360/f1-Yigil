a461f6bcec463fca694cdfab4f5b158c4a8adc1f
Jung Wook Park <hatchling3713@gmail.com>
2024-01-30 14:48:04 +0900

    test: 테스트 오류 수정

--- frontend/__tests__/SearchBar.test.tsx
+++ frontend/__tests__/SearchBar.test.tsx
@@ -1,9 +1,8 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-import { SearchBar } from '@/app/_components/search';
+import SearchBar from '@/app/_components/search/SearchBar';
 import { describe, expect, it } from 'vitest';
-import { ReadonlyURLSearchParams } from 'next/navigation';
 
 vi.mock('next/navigation', () => {
   return {
@@ -19,7 +18,10 @@ vi.mock('next/navigation', () => {
 
 describe('SearchBar', () => {
   it('renders cancellable SearchBar', () => {
-    render(<SearchBar cancellable />);
+    //mocking prop
+    const addResult = (result: string) => {};
+
+    render(<SearchBar addResult={addResult} cancellable />);
 
     const button = screen.getByText('취소');
 
