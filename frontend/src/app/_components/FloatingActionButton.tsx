9b50d65428f7642d79ae7a9b23376724f3781c32
davidktlee <rudxor567@naver.com>
2024-02-01 20:18:44 +0900

    feat: FAB 버튼에 팝 오버 기능 추가

--- frontend/src/app/_components/FloatingActionButton.tsx
+++ frontend/src/app/_components/FloatingActionButton.tsx
@@ -1,13 +1,48 @@
 'use client';
 
 import PlusIcon from '@/../public/icons/plus.svg';
+import { useState } from 'react';
+import PopOver from './ui/popover/PopOver';
+import LocationIcon from '/public/icons/location.svg';
+import CalendarIcon from '/public/icons/calendar.svg';
+
+const popOverData = [
+  { href: '/add/spot', label: '장소 추가하기', Icon: LocationIcon },
+  { href: '/add/course', label: '일정 추가하기', Icon: CalendarIcon },
+];
 
 export default function FloatingActionButton() {
+  const [modalOpened, setIsModalOpened] = useState(false);
+
+  const closeModal = () => {
+    setIsModalOpened(false);
+  };
+
   return (
-    <span className="w-min absolute right-20">
-      <button className="fixed bottom-4 w-16 h-16 bg-blue-600 rounded-full flex justify-center items-center">
-        <PlusIcon />
+    <div className="w-min absolute right-20">
+      <button
+        className={`fixed bottom-4 w-16 h-16 rounded-full flex justify-center items-center  ${
+          modalOpened ? 'bg-black cursor-auto' : 'bg-blue-600'
+        }`}
+        onClick={() => setIsModalOpened(!modalOpened)}
+      >
+        <PlusIcon
+          className={`${
+            modalOpened
+              ? 'rotate-45 duration-1000 z-30'
+              : 'rotate-0 duration-1000'
+          } `}
+        />
+        {modalOpened && (
+          <PopOver
+            popOverData={popOverData}
+            closeModal={closeModal}
+            position="bottom-[70px] right-4"
+            backDropStyle="bg-black bg-opacity-25"
+            style="w-[160px] z-30"
+          />
+        )}
       </button>
-    </span>
+    </div>
   );
 }
