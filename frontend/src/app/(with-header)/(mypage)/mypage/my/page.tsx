e1b5e04721071384dfae6bd29ededb159ce60878
davidktlee <rudxor567@naver.com>
2024-02-03 22:34:31 +0900

    fix: 라우팅 구조 변경

--- frontend/src/app/(with-header)/(mypage)/mypage/my/page.tsx
+++
@@ -1,6 +0,0 @@
-import MyPageContent from '@/app/_components/mypage/routeTabs/MyPageTabs';
-import React from 'react';
-
-export default function MyPageMy() {
-  return <div className=""></div>;
-}
--- frontend/src/app/(with-header)/(mypage)/mypage/page.tsx
+++
@@ -1,9 +0,0 @@
-import MyPageContent from '@/app/_components/mypage/routeTabs/MyPageTabs';
-import MyPageInfo from '@/app/_components/mypage/MyPageInfo';
-import React from 'react';
-/**
- * TODO: FAB 버튼 추가
- */
-export default function MyPage() {
-  return <div></div>;
-}
