61813d3a05fd40e577f228dfa38eb6d17ea86749
StoneHee99 <kiit0901@gmail.com>
2024-02-05 12:33:24 +0900

    add 404 page for none routed pages

--- admin-frontend/yigil-admin/src/App.tsx
+++ admin-frontend/yigil-admin/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage";
 //import "./App.css";
 import LoginPage from "./components/login/LoginPage";
 import AdminSignUpPage from "./components/admin_sign_up/AdminSignUpPage";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminSignUpPage />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
   );
