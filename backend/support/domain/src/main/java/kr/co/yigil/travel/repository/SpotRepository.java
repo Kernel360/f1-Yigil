c8e522ccf52b420ae4ccd48b599b6d5712a45549
Uknow928 <youknow928@yonsei.ac.kr>
2024-02-05 18:23:36 +0900

    도메인 요구사항에 맞게 spot, place, course, travel 수정

--- backend/support/domain/src/main/java/kr/co/yigil/travel/repository/SpotRepository.java
+++ backend/support/domain/src/main/java/kr/co/yigil/travel/repository/SpotRepository.java
@@ -7,6 +7,7 @@ import kr.co.yigil.travel.Spot;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +16,7 @@ public interface SpotRepository extends JpaRepository<Spot, Long> {
 
     Optional<Spot> findByIdAndMemberId(Long spotId, Long memberId);
 
+    @Query("SELECT s FROM Spot s WHERE s.place.id = :placeId AND s.isInCourse = false")
     Slice<Spot> findAllByPlaceIdAndIsInCourseFalse(@Param("placeId") Long placeId, Pageable pageable);
-
-    List<Spot> findAllByMember(Member member);
+        List<Spot> findAllByMember(Member member);
 }
