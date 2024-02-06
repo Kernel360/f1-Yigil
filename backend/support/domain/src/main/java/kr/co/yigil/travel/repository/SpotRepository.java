0fd1e9ed45d55e9913ed17ca1695707b6d3d6cec
minson96 <vlrmakdhkd@gmail.com>
2024-02-06 16:33:45 +0900

    Member 도메인 수정 및 Follow Service 수정

--- backend/support/domain/src/main/java/kr/co/yigil/travel/repository/SpotRepository.java
+++ backend/support/domain/src/main/java/kr/co/yigil/travel/repository/SpotRepository.java
@@ -1,9 +1,10 @@
 package kr.co.yigil.travel.repository;
 
-import java.util.List;
+
 import java.util.Optional;
 import kr.co.yigil.member.Member;
 import kr.co.yigil.travel.Spot;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,4 +19,7 @@ public interface SpotRepository extends JpaRepository<Spot, Long> {
 
     @Query("SELECT s FROM Spot s WHERE s.place.id = :placeId AND s.isInCourse = false")
     Slice<Spot> findAllByPlaceIdAndIsInCourseFalse(@Param("placeId") Long placeId, Pageable pageable);
+
+    @Query("SELECT s FROM Spot s WHERE s.member.id = :memberId AND s.isInCourse = false")
+    Slice<Spot> findAllByMemberAndIsInCourseFalse(Long memberId, Pageable pageable);
 }
