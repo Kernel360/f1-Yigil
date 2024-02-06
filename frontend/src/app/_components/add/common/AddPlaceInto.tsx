d2135b051930df302fb833f6806fbf7d932979a5
Jung Wook Park <hatchling3713@gmail.com>
2024-02-06 21:36:19 +0900

    design: 장소 정보 컴포넌트 레이아웃 작성

---
+++ frontend/src/app/_components/add/common/AddPlaceInto.tsx
@@ -0,0 +1,29 @@
+const placeholderImageUrl = 'https://placehold.co/400x300';
+
+// 이름과 주소를 입력으로 하여 static map 저장 여부를 반환하는 API가 필요함
+
+// 외부 placeholder 이미지 사용중, no-img-element 린트 에러 발생
+// 차후 next/image 사용하게 변경 예정
+export default function AddPlaceInfo({
+  name,
+  address,
+}: {
+  name: string;
+  address: string;
+}) {
+  return (
+    <section className="p-4 flex flex-col gap-8">
+      <div className="px-4 flex justify-between items-center">
+        <span className="text-gray-500">이름</span>
+        <span className="font-medium text-2xl">{name}</span>
+      </div>
+      <div className="px-4 flex justify-between items-center">
+        <span className="text-gray-500">주소</span>
+        <span className="font-medium">{address}</span>
+      </div>
+      <div className="w-full relative">
+        <img src={placeholderImageUrl} alt="Example static map" />
+      </div>
+    </section>
+  );
+}
