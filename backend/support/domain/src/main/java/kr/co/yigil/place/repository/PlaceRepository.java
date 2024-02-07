0dfb4bcaa3b95ba714b206d2b64600fc77ac7bbc
Uknow928 <youknow928@yonsei.ac.kr>
2024-02-07 22:43:58 +0900

    fix error

--- backend/support/domain/src/main/java/kr/co/yigil/place/repository/PlaceRepository.java
+++ backend/support/domain/src/main/java/kr/co/yigil/place/repository/PlaceRepository.java
@@ -5,11 +5,12 @@ import kr.co.yigil.place.Place;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     Optional<Place> findByName(String name);
     Optional<Place> findByNameAndAddress(String name, String address);
-
+    @Query("SELECT p FROM Place p")
     Slice<Place>  findAllPlaces(Pageable pageable);
 }
