a72e1cd24d25122f4d07fcf8c6a308774e53a082
StoneHee99 <kiit0901@gmail.com>
2024-02-07 17:11:56 +0900

    add server config

--- admin-frontend/yigil-admin/vite.config.ts
+++ admin-frontend/yigil-admin/vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
