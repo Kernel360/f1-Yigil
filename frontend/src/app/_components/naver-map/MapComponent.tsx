a56c5526e7bad1ef55aab74788407b7155c78070
davidktlee <rudxor567@naver.com>
2024-02-07 20:08:44 +0900

    init: 리액트 네이버 지도 install

---
+++ frontend/src/app/_components/naver-map/MapComponent.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import { NaverMap } from 'react-naver-maps';
+
+export default function MapComponent() {
+  return (
+    <>
+      <NaverMap></NaverMap>
+    </>
+  );
+}
