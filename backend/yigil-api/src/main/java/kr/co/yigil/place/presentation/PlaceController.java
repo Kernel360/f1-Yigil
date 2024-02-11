f000c48416ab5b3602d3033b422457103e7fe5f7
YunNote <zzdd1558@gmail.com>
2024-02-12 03:34:45 +0900

    feature: 통일성 필요

--- backend/yigil-api/src/main/java/kr/co/yigil/place/presentation/PlaceController.java
+++ backend/yigil-api/src/main/java/kr/co/yigil/place/presentation/PlaceController.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+
+//
 @RequestMapping("/api/v1/places")
 public class PlaceController {
 
