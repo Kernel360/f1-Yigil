66b3f0c56b5d825c13013af32b32c8325814bcfa
davidktlee <rudxor567@naver.com>
2024-02-16 22:20:53 +0900

    refactor: http request 서버 액션으로 변경

--- frontend/src/app/_components/api/httpRequest.ts
+++ frontend/src/app/_components/api/httpRequest.ts
@@ -1,26 +1,37 @@
+'use server';
+
 const BASE_URL =
   typeof window !== 'undefined'
     ? process.env.NEXT_PUBLIC_BASE_URL
     : process.env.BASE_URL;
 
-const headerInitOption = { 'Content-Type': 'application/json' };
+const headerInitOption = {
+  'Content-Type': 'application/json',
+};
+
 export const httpRequest =
   (url: string) =>
   (params: string = '') =>
   (method: string = 'GET', body: any = null) =>
-  (headers: {} = headerInitOption) =>
+  (headers: Record<string, string> = headerInitOption) =>
   async (errorMsg: string = '요청에 실패했습니다') => {
     try {
       const res = await fetch(`${BASE_URL}/v1/${url}${params}`, {
         method,
-        headers,
+        headers: {
+          ...headerInitOption,
+          ...headers,
+        },
+
         ...(body && { body: JSON.stringify(body) }),
       });
 
-      if (res.ok) {
-        const result = await res.json();
-        return result;
-      }
+      return res.json();
+
+      // if (res.ok) {
+      //   const result = await res.json();
+      //   return result;
+      // }
     } catch (error) {
       // error 컴포넌트나 토스트 정의 시 사용할 부분
       console.log(error); // 디버그 용
