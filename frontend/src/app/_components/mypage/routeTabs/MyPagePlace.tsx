53d921874603d5dd9ff0abcf011d70317d3d8582
davidktlee <rudxor567@naver.com>
2024-02-21 23:23:46 +0900

    feat: 라우트 추가 및 checkPath 함수로 path와 href 값 비교에 따른 스타일링 처리

--- frontend/src/app/_components/mypage/routeTabs/MyPagePlace.tsx
+++ frontend/src/app/_components/mypage/routeTabs/MyPagePlace.tsx
@@ -2,20 +2,21 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
-import { myPageMyPlaceTab } from '../constants';
+import { myPageMyTab } from '../constants';
+import { checkPath } from './MyPageRoutes';
 
 export default function MyPagePlace() {
   const path = usePathname();
 
   return (
     <div className="items-center grid grid-cols-4">
-      {myPageMyPlaceTab.map(({ href, label }, idx) => (
+      {myPageMyTab.map(({ href, label }, idx) => (
         <Link
           href={href}
           key={idx}
           tabIndex={0}
           className={`py-[14px] flex justify-center text-xl leading-6 cursor-pointer ${
-            path === href
+            checkPath(path, href, 2)
               ? 'text-gray-700 font-semibold'
               : 'text-gray-300 font-normal'
           } `}
