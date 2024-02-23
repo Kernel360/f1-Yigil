0439b8e0e85a898b6681b805716a85dea74cc912
StoneHee99 <kiit0901@gmail.com>
2024-02-23 17:22:38 +0900

    fix

--- backend/yigil-api/src/main/java/kr/co/yigil/travel/application/CourseFacade.java
+++ backend/yigil-api/src/main/java/kr/co/yigil/travel/application/CourseFacade.java
@@ -6,7 +6,6 @@ import kr.co.yigil.travel.domain.course.CourseCommand;
 import kr.co.yigil.travel.domain.course.CourseCommand.ModifyCourseRequest;
 import kr.co.yigil.travel.domain.course.CourseCommand.RegisterCourseRequest;
 import kr.co.yigil.travel.domain.course.CourseCommand.RegisterCourseRequestWithSpotInfo;
-import kr.co.yigil.travel.domain.course.CourseInfo;
 import kr.co.yigil.travel.domain.course.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
