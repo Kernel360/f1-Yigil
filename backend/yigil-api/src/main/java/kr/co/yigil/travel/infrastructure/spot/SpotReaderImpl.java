95567c968ef2c19ff0aadcbdafba716e679dd9a9
ShineCorine <imsjm81@gmail.com>
2024-02-29 10:19:18 +0900

    spotReaderImpl 의 getMemberSpotList 에서 querydsl 상요하도록 변경

--- backend/yigil-api/src/main/java/kr/co/yigil/travel/infrastructure/spot/SpotReaderImpl.java
+++ backend/yigil-api/src/main/java/kr/co/yigil/travel/infrastructure/spot/SpotReaderImpl.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import kr.co.yigil.global.exception.BadRequestException;
 import kr.co.yigil.travel.domain.Spot;
 import kr.co.yigil.travel.domain.spot.SpotReader;
+import kr.co.yigil.travel.infrastructure.SpotQueryDslRepository;
 import kr.co.yigil.travel.infrastructure.SpotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +21,7 @@ import org.springframework.stereotype.Component;
 public class SpotReaderImpl implements SpotReader {
 
     private final SpotRepository spotRepository;
+    private final SpotQueryDslRepository spotQueryDslRepository;
 
     @Override
     public Spot getSpot(Long spotId) {
@@ -58,9 +60,10 @@ public class SpotReaderImpl implements SpotReader {
     @Override
     public Page<Spot> getMemberSpotList(Long memberId, Pageable pageable,
         String visibility) {
-        if (visibility.equals("all")) {
-            return spotRepository.findAllByMemberId(memberId, pageable);
-        }
-        return spotRepository.findAllByMemberIdAndIsPrivate(memberId, visibility.equals("private"), pageable);
+        return spotQueryDslRepository.findAllByMemberIdAndIsPrivate(memberId, visibility, pageable);
+//        if (visibility.equals("all")) {
+//            return spotRepository.findAllByMemberId(memberId, pageable);
+//        }
+//        return spotRepository.findAllByMemberIdAndIsPrivate(memberId, visibility.equals("private"), pageable);
     }
 }
