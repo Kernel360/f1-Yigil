13c1fcc9e57285615a26d354394045106a9ed6fe
Jung Wook Park <hatchling3713@gmail.com>
2024-03-01 00:08:08 +0900

    fix: 변경된 로그인 기능에 맞춰 로그아웃 구현

---
+++ frontend/src/app/_components/api/action.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { z } from 'zod';
+import { cookies } from 'next/headers';
+import { requestWithCookie } from './httpRequest';
+
+export async function logout() {
+  const response = await requestWithCookie('logout')()()()(
+    '로그아웃에 실패했습니다!',
+  );
+
+  const result = logoutSuccessResponse.safeParse(response);
+
+  if (!result.success) {
+    console.log(result.error.message);
+    console.error(result.error.message);
+    return;
+  }
+
+  cookies().delete('SESSION');
+}
+
+const logoutSuccessResponse = z.object({
+  message: z.string(),
+});
