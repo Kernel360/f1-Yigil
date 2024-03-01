246745c547340a609bbe069226a1f97fee467824
ShineCorine <imsjm81@gmail.com>
2024-03-01 23:39:36 +0900

    api 문서 업데이트

--- backend/yigil-api/src/test/java/kr/co/yigil/comment/interfaces/controller/CommentApiControllerTest.java
+++ backend/yigil-api/src/test/java/kr/co/yigil/comment/interfaces/controller/CommentApiControllerTest.java
@@ -153,7 +153,6 @@ class CommentApiControllerTest {
                     parameterWithName("size").description("페이지 크기")
                 ),
                 responseFields(
-                    fieldWithPath("has_next").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부"),
                     subsectionWithPath("content").description("comment의 정보"),
                     fieldWithPath("content[].id").type(JsonFieldType.NUMBER).description("댓글 id"),
                     fieldWithPath("content[].content").type(JsonFieldType.STRING).description("댓글 내용"),
@@ -161,7 +160,8 @@ class CommentApiControllerTest {
                     fieldWithPath("content[].member_nickname").type(JsonFieldType.STRING).description("닉네임"),
                     fieldWithPath("content[].member_image_url").type(JsonFieldType.STRING).description("프로필 이미지 url"),
                     fieldWithPath("content[].child_count").type(JsonFieldType.NUMBER).description("자식 댓글 수"),
-                    fieldWithPath("content[].created_at").type(JsonFieldType.STRING).description("생성일")
+                    fieldWithPath("content[].created_at").type(JsonFieldType.STRING).description("생성일"),
+                    fieldWithPath("has_next").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부")
                 )
             ));
     }
@@ -171,11 +171,11 @@ class CommentApiControllerTest {
     void whenGetChildCommentList_thenReturns200AndCommentResponse() throws Exception {
 
             CommentDto.CommentsUnitInfo mockUnitInfo1 = new CommentDto.CommentsUnitInfo(
-                1L, "content", 1L, "nickname", "http://yigil.co.kr/images/profile.jpg", 3,
+                1L, "content", 1L, "nickname", "http://yigil.co.kr/images/profile.jpg", 0,
                 "2024-03-01");
 
             CommentDto.CommentsUnitInfo mockUnitInfo2 = new CommentDto.CommentsUnitInfo(
-                2L, "content2", 1L, "nickname3", "http://yigil.co.kr/images/profile3.jpg", 1,
+                2L, "content2", 1L, "nickname3", "http://yigil.co.kr/images/profile3.jpg", 0,
                 "2024-03-01");
             List<CommentDto.CommentsUnitInfo> commentsUnitInfoList = List.of(mockUnitInfo1,
                 mockUnitInfo2);
@@ -205,7 +205,7 @@ class CommentApiControllerTest {
                         parameterWithName("size").description("페이지 크기")
                     ),
                     responseFields(
-                        fieldWithPath("has_next").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부"),
+
                         subsectionWithPath("content").description("comment의 정보"),
                         fieldWithPath("content[].id").type(JsonFieldType.NUMBER).description("댓글 id"),
                         fieldWithPath("content[].content").type(JsonFieldType.STRING).description("댓글 내용"),
@@ -213,7 +213,8 @@ class CommentApiControllerTest {
                         fieldWithPath("content[].member_nickname").type(JsonFieldType.STRING).description("닉네임"),
                         fieldWithPath("content[].member_image_url").type(JsonFieldType.STRING).description("프로필 이미지"),
                         fieldWithPath("content[].child_count").type(JsonFieldType.NUMBER).description("자식 댓글 수"),
-                        fieldWithPath("content[].created_at").type(JsonFieldType.STRING).description("생성일")
+                        fieldWithPath("content[].created_at").type(JsonFieldType.STRING).description("생성일"),
+                        fieldWithPath("has_next").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부")
                     )
                 ));
     }
@@ -245,6 +246,9 @@ class CommentApiControllerTest {
                     fieldWithPath("content").description("댓글 내용"),
                     fieldWithPath("parentId").description("부모 댓글 id"),
                     fieldWithPath("notifiedMemberId").description("알림을 받을 회원 id")
+                ),
+                responseFields(
+                    fieldWithPath("message").description("응답 메시지")
                 )
             ));
 
