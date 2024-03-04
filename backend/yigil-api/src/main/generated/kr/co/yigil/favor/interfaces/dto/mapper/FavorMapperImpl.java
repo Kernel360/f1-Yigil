70d42c945dd5135cf0e5fd7cc266d62fae202401
StoneHee99 <kiit0901@gmail.com>
2024-03-04 18:30:39 +0900

    refresh

---
+++ backend/yigil-api/src/main/generated/kr/co/yigil/favor/interfaces/dto/mapper/FavorMapperImpl.java
@@ -0,0 +1,41 @@
+package kr.co.yigil.favor.interfaces.dto.mapper;
+
+import javax.annotation.processing.Generated;
+import kr.co.yigil.favor.domain.FavorInfo;
+import kr.co.yigil.favor.interfaces.dto.FavorDto;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-03-04T18:24:58+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+)
+@Component
+public class FavorMapperImpl implements FavorMapper {
+
+    @Override
+    public FavorDto.AddFavorResponse of(FavorInfo.AddFavorResponse response) {
+        if ( response == null ) {
+            return null;
+        }
+
+        FavorDto.AddFavorResponse.AddFavorResponseBuilder addFavorResponse = FavorDto.AddFavorResponse.builder();
+
+        addFavorResponse.message( response.getMessage() );
+
+        return addFavorResponse.build();
+    }
+
+    @Override
+    public FavorDto.DeleteFavorResponse of(FavorInfo.DeleteFavorResponse response) {
+        if ( response == null ) {
+            return null;
+        }
+
+        FavorDto.DeleteFavorResponse.DeleteFavorResponseBuilder deleteFavorResponse = FavorDto.DeleteFavorResponse.builder();
+
+        deleteFavorResponse.message( response.getMessage() );
+
+        return deleteFavorResponse.build();
+    }
+}
