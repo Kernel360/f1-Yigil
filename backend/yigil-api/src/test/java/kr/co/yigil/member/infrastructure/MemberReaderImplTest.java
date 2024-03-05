9257344a03bf77cdec565839dc548f4c65dd64e3
StoneHee99 <kiit0901@gmail.com>
2024-03-05 10:53:20 +0900

    add test for new code

--- backend/yigil-api/src/test/java/kr/co/yigil/member/infrastructure/MemberReaderImplTest.java
+++ backend/yigil-api/src/test/java/kr/co/yigil/member/infrastructure/MemberReaderImplTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 import kr.co.yigil.member.Member;
+import kr.co.yigil.member.SocialLoginType;
 import kr.co.yigil.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,14 +37,41 @@ class MemberReaderImplTest {
         assertThat(result).isEqualTo(member);
     }
 
-    @DisplayName("gvalidateMember 메서드가 올바른 응답을 반환하는지")
+    @DisplayName("validateMember 메서드가 올바른 응답을 반환하는지")
     @Test
     void WhenValidateMember_ThenShouldNotOccuredErrror() {
-
         Long memberid = 1L;
         when(memberRepository.existsById(memberid)).thenReturn(true);
 
         memberReader.validateMember(memberid);
-        
+    }
+
+    @DisplayName("findMemberBySocialLoginIdAndSocialLoginType 메서드가 올바른 응답을 반환하는지")
+    @Test
+    void WhenFindMemberBySocialLoginIdAndSocialLoginType_ThenShouldReturnMember() {
+        String socialLoginId = "socialLoginId";
+        var socialLoginType = mock(SocialLoginType.class);
+        Member member = mock(Member.class);
+
+        when(memberRepository.findMemberBySocialLoginIdAndSocialLoginType(socialLoginId, socialLoginType)).thenReturn(Optional.of(member));
+
+        var result = memberReader.findMemberBySocialLoginIdAndSocialLoginType(socialLoginId, socialLoginType);
+
+        assertThat(result).isEqualTo(Optional.of(member));
+    }
+
+    @DisplayName("findMemberByEmailAndSocialLoginType 메서드가 올바른 응답을 반환하는지")
+    @Test
+    void whenFindMemberByEmailAndSocialLoginType_ThenShouldReturnMember() {
+        String email = "email";
+        var socialLoginType = mock(SocialLoginType.class);
+        Member member = mock(Member.class);
+
+        when(memberRepository.findMemberByEmailAndSocialLoginType(email,
+                socialLoginType)).thenReturn(Optional.of(member));
+
+        var result = memberReader.findMemberByEmailAndSocialLoginType(email, socialLoginType);
+
+        assertThat(result).isEqualTo(Optional.of(member));
     }
 }
