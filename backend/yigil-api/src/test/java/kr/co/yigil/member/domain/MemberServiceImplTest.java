d4e89c0b34856ec3d3d9499d176cc0cb72cba9f2
ShineCorine <imsjm81@gmail.com>
2024-03-09 00:02:47 +0900

    닉네임 중복 테스트 코드

--- backend/yigil-api/src/test/java/kr/co/yigil/member/domain/MemberServiceImplTest.java
+++ backend/yigil-api/src/test/java/kr/co/yigil/member/domain/MemberServiceImplTest.java
@@ -1,22 +1,11 @@
 package kr.co.yigil.member.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
-import kr.co.yigil.file.AttachFile;
-import kr.co.yigil.file.FileUploader;
-import kr.co.yigil.follow.domain.FollowCount;
-import kr.co.yigil.follow.domain.FollowReader;
-import kr.co.yigil.member.Member;
-import kr.co.yigil.member.domain.MemberCommand.MemberUpdateRequest;
-import kr.co.yigil.region.domain.RegionReader;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,6 +14,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+
+import kr.co.yigil.file.AttachFile;
+import kr.co.yigil.file.FileUploader;
+import kr.co.yigil.follow.domain.FollowCount;
+import kr.co.yigil.follow.domain.FollowReader;
+import kr.co.yigil.member.Member;
+import kr.co.yigil.member.domain.MemberCommand.MemberUpdateRequest;
+import kr.co.yigil.region.domain.RegionReader;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceImplTest {
@@ -88,4 +85,14 @@ class MemberServiceImplTest {
         memberService.updateMemberInfo(memberId, request);
         verify(mockMember).updateMemberInfo(anyString(), anyString(), anyString(), any(AttachFile.class), anyList());
     }
+
+    @DisplayName("nicknameDuplicateCheck 를 호출했을 때 닉네임 중복 체크가 잘 되는지 확인")
+	@Test
+	void whenNicknameDuplicateCheck() {
+        String nickname = "nickname";
+        when(memberReader.existsByNickname(nickname)).thenReturn(false);
+        MemberInfo.NicknameCheckInfo result = memberService.nicknameDuplicateCheck(nickname);
+        assertThat(result).isNotNull().isInstanceOf(MemberInfo.NicknameCheckInfo.class);
+        assertThat(result.isAvailable()).isTrue();
+	}
 }
