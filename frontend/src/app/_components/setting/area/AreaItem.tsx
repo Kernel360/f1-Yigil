2fa04d270a8ba544d41c8e4e59c97fabf143c122
davidktlee <rudxor567@naver.com>
2024-03-10 00:47:11 +0900

    feat: 마이페이지 선호 지역 선택 부분까지 완료. api 연결 필요

--- frontend/src/app/_components/setting/area/AreaItem.tsx
+++ frontend/src/app/_components/setting/area/AreaItem.tsx
@@ -1,24 +1,53 @@
 import { TMyPageRegions } from '@/types/myPageResponse';
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import StarIcon from '/public/icons/star.svg';
 import { EventFor } from '@/types/type';
 
-export default function AreaItem({ regions }: { regions: TMyPageRegions[] }) {
-  const onClickStar = (e: EventFor<'li', 'onClick'>) => {
+export default function AreaItem({
+  regions,
+  selectedRegions,
+  setSelectedRegions,
+}: {
+  regions: TMyPageRegions[];
+  selectedRegions: number[];
+  setSelectedRegions: Dispatch<SetStateAction<number[]>>;
+}) {
+  const onClickStar = (e: EventFor<'li', 'onClick'>, id: number) => {
     e.stopPropagation();
+    if (selectedRegions.includes(id)) {
+      const deleteRegion = selectedRegions.filter((region) => region !== id);
+      setSelectedRegions(deleteRegion);
+    } else {
+      setSelectedRegions((prev) => [...prev, id]);
+    }
+  };
+
+  const onKeyDownEnter = (e: EventFor<'li', 'onKeyDown'>, id: number) => {
+    if (e.key === 'Enter') {
+      if (selectedRegions.includes(id)) {
+        const deleteRegion = selectedRegions.filter((region) => region !== id);
+        setSelectedRegions(deleteRegion);
+      } else {
+        setSelectedRegions((prev) => [...prev, id]);
+      }
+    }
   };
   return (
     <ul className="grid grid-cols-2 bg-gray-100 py-4">
       {regions.map(({ id, region_name, selected }) => (
         <li
           key={id}
-          className="flex justify-center items-center gap-x-1 py-1 my-2"
-          onClick={onClickStar}
+          tabIndex={0}
+          className="flex justify-center items-center gap-x-1 py-1 my-2 cursor-pointer"
+          onClick={(e) => onClickStar(e, id)}
+          onKeyDown={(e) => onKeyDownEnter(e, id)}
         >
           <div className={`text-gray-400 leading-none`}>{region_name}</div>
           <StarIcon
             className={`w-5 h-5 mb-[3px] ${
-              selected ? 'fill-[#FACC15] stroke-[#FACC15]' : 'stroke-gray-400'
+              selectedRegions.includes(id)
+                ? 'fill-[#FACC15] stroke-[#FACC15]'
+                : 'stroke-gray-400'
             } `}
           />
         </li>
--- frontend/src/app/_components/setting/area/AreaList.tsx
+++ frontend/src/app/_components/setting/area/AreaList.tsx
@@ -1,18 +1,23 @@
 'use client';
 import { TMyPageAllArea } from '@/types/myPageResponse';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChevronDown from '/public/icons/chevron-down.svg';
 import AreaItem from './AreaItem';
 
 export default function AreaList({ categories }: TMyPageAllArea) {
-  const counts = useMemo(() => {
-    return categories.flatMap((category) =>
-      category.regions.filter((region) => region.selected),
-    ).length;
-  }, [categories]);
-
   const [clickedRegion, setClickedRegion] = useState('');
+  const [selectedRegions, setSelectedRegions] = useState<number[]>([]);
 
+  useEffect(() => {
+    const regionIds = categories
+      .flatMap((category) =>
+        category.regions.filter((region) => {
+          if (region.selected) return region.id;
+        }),
+      )
+      .map((region) => region.id);
+    setSelectedRegions(regionIds);
+  }, [categories]);
   const changeDropdownRegion = (regionName: string) => {
     setClickedRegion(regionName);
     if (clickedRegion === regionName) setClickedRegion('');
@@ -26,44 +31,96 @@ export default function AreaList({ categories }: TMyPageAllArea) {
   return (
     <section className="mx-6">
       <div className="flex justify-end items-center text-gray-400">
-        <div className="text-2xl leading-7 mr-1">{counts} /</div>
+        <div className="text-2xl leading-7 mr-1">
+          {selectedRegions.length} /
+        </div>
         <div className="text-xl leading-6">5</div>
       </div>
-      <ul className={`flex flex-wrap justify-between mt-3`}>
-        {categories.map(({ category_name, regions }, idx) => (
-          <>
-            <li
-              tabIndex={0}
-              key={category_name}
-              className={`cursor-pointer basis-[47%] mt-6 order-${idx} shrink-0`}
-              onKeyDown={(e) =>
-                e.key === 'Enter' && onKeyDownEnter(category_name)
-              }
-              onClick={() => changeDropdownRegion(category_name)}
-            >
-              <ul
-                className={`flex justify-between items-center px-2 py-3 relative ${
-                  clickedRegion === category_name
-                    ? 'bg-gray-100 border-b-2 border-b-gray-100'
-                    : 'border-b-2 border-b-gray-500'
-                }`}
-              >
-                <div>{category_name}</div>
-                {clickedRegion === category_name ? (
-                  <ChevronDown className="w-4 h-4 stroke-gray-500 rotate-180" />
-                ) : (
-                  <ChevronDown className="w-4 h-4 stroke-gray-500" />
+      <div className="mt-3">
+        <table className="w-full">
+          <tbody>
+            {categories.map(({ category_name, regions }, idx) => (
+              <React.Fragment key={category_name}>
+                {idx % 2 === 0 && (
+                  <tr className="flex gap-x-6">
+                    <td
+                      tabIndex={0}
+                      className="cursor-pointer basis-[48%]"
+                      onKeyDown={(e) =>
+                        e.key === 'Enter' && onKeyDownEnter(category_name)
+                      }
+                      onClick={() => changeDropdownRegion(category_name)}
+                    >
+                      <div
+                        className={`flex justify-between items-center px-2 py-3 mt-4  ${
+                          clickedRegion === category_name
+                            ? 'bg-gray-100 border-b-[1px] border-b-gray-100'
+                            : 'border-b-[1px] border-b-gray-500'
+                        }`}
+                      >
+                        <div>{category_name}</div>
+                        {clickedRegion === category_name ? (
+                          <ChevronDown className="w-4 h-4 stroke-gray-500 rotate-180" />
+                        ) : (
+                          <ChevronDown className="w-4 h-4 stroke-gray-500" />
+                        )}
+                      </div>
+                    </td>
+                    {categories[idx + 1] && (
+                      <td
+                        tabIndex={0}
+                        className={`cursor-pointer basis-[48%] `}
+                        onKeyDown={(e) =>
+                          e.key === 'Enter' &&
+                          onKeyDownEnter(categories[idx + 1].category_name)
+                        }
+                        onClick={() =>
+                          changeDropdownRegion(
+                            categories[idx + 1].category_name,
+                          )
+                        }
+                      >
+                        <div
+                          className={`flex justify-between items-center px-2 py-3 mt-4 ${
+                            clickedRegion === categories[idx + 1].category_name
+                              ? 'bg-gray-100 border-b-[1px] border-b-gray-100'
+                              : 'border-b-[1px] border-b-gray-500'
+                          } `}
+                        >
+                          <div>{categories[idx + 1].category_name}</div>
+                          {clickedRegion ===
+                          categories[idx + 1].category_name ? (
+                            <ChevronDown className="w-4 h-4 stroke-gray-500 rotate-180" />
+                          ) : (
+                            <ChevronDown className="w-4 h-4 stroke-gray-500" />
+                          )}
+                        </div>
+                      </td>
+                    )}
+                  </tr>
                 )}
-              </ul>
-            </li>
-            {clickedRegion === category_name && (
-              <div className={`w-full ${idx % 2 === 0 && `order-${idx + 2}`}`}>
-                <AreaItem regions={regions} />
-              </div>
-            )}
-          </>
-        ))}
-      </ul>
+                {clickedRegion === category_name && (
+                  <tr>
+                    <td colSpan={2}>
+                      <div className="w-full">
+                        <AreaItem
+                          regions={regions}
+                          selectedRegions={selectedRegions}
+                          setSelectedRegions={setSelectedRegions}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
+
+/**
+ *
+ */
