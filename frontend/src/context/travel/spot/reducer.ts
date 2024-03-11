7862d405e9bbd9eb76a8e56e0926f1711dae1d60
Jung Wook Park <hatchling3713@gmail.com>
2024-03-11 18:26:00 +0900

    refactor: Spot 추가를 위한 reducer 동작 및 테스트 작성

--- frontend/src/context/travel/spot/reducer.ts
+++ frontend/src/context/travel/spot/reducer.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
-import type { TImageData } from '@/app/_components/images/ImageHandler';
+import { inputImageSchema } from '@/app/_components/images';
+
+import type { TInputImage } from '@/app/_components/images';
 
 const choosePlaceSchema = z.object({
   name: z.string(),
@@ -9,13 +11,19 @@ const choosePlaceSchema = z.object({
   coords: z.object({ lng: z.number(), lat: z.number() }),
 });
 
-type TChoosePlace = z.infer<typeof choosePlaceSchema>;
+export type TChoosePlace = z.infer<typeof choosePlaceSchema>;
+
+const reviewSchema = z.object({
+  rate: z.number().int().gte(1).lte(5),
+  content: z.string().max(30),
+});
+
+export type TSpotReview = z.infer<typeof reviewSchema>;
 
 export interface TSpotState {
   place: TChoosePlace;
-  images: TImageData[];
-  rating: 1 | 2 | 3 | 4 | 5;
-  review: { title?: string; content: string };
+  images: TInputImage[];
+  review: TSpotReview;
 }
 
 export interface TSpotAction {
@@ -26,20 +34,49 @@ export interface TSpotAction {
 export const initialSpotState: TSpotState = {
   place: { name: '', address: '', mapImageUrl: '', coords: { lng: 0, lat: 0 } },
   images: [],
-  rating: 1,
-  review: { content: '' },
+  review: { rate: 1, content: '' },
 };
 
 export function reducer(state: TSpotState, action: TSpotAction): TSpotState {
   switch (action.type) {
     case 'SET_PLACE': {
-      return { ...state };
+      const result = choosePlaceSchema.safeParse(action.payload);
+
+      /**
+       * @todo SET_ERROR for Toast
+       */
+      if (!result.success) {
+        console.error(result.error.message);
+        return { ...state };
+      }
+
+      return { ...state, place: result.data };
     }
     case 'SET_IMAGES': {
-      return { ...state };
+      const result = z.array(inputImageSchema).safeParse(action.payload);
+
+      /**
+       * @todo SET_ERROR for Toast
+       */
+      if (!result.success) {
+        console.error(result.error.message);
+        return { ...state };
+      }
+
+      return { ...state, images: result.data };
     }
     case 'SET_REVIEW': {
-      return { ...state };
+      const result = reviewSchema.safeParse(action.payload);
+
+      /**
+       * @todo SET_ERROR for Toast
+       */
+      if (!result.success) {
+        console.error(result.error.message);
+        return { ...state };
+      }
+
+      return { ...state, review: result.data };
     }
   }
 }
