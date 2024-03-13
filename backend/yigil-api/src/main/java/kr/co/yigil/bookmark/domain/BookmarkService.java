0db4a99e45381a51d4872b441374226d36fc006c
ShineCorine <imsjm81@gmail.com>
2024-03-13 23:56:04 +0900

    북마크 이미지 cdn 수정

--- backend/yigil-api/src/main/java/kr/co/yigil/bookmark/domain/BookmarkService.java
+++ backend/yigil-api/src/main/java/kr/co/yigil/bookmark/domain/BookmarkService.java
@@ -1,6 +1,5 @@
 package kr.co.yigil.bookmark.domain;
 
-import kr.co.yigil.bookmark.domain.dto.BookmarkDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 
@@ -10,5 +9,5 @@ public interface BookmarkService {
 
     void deleteBookmark(Long memberId, Long placeId);
 
-    Slice<BookmarkDto> getBookmarkSlice(Long memberId, PageRequest pageRequest);
+    Slice<BookmarkInfo> getBookmarkSlice(Long memberId, PageRequest pageRequest);
 }
