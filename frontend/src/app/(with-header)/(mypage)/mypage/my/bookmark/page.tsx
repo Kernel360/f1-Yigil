86deffa4462aaea406e19e2df54261cbc11c251c
davidktlee <rudxor567@naver.com>
2024-03-15 09:56:21 +0900

    feat: 북마크 페이지 데이터 페칭 실패 화면 설정

--- frontend/src/app/(with-header)/(mypage)/mypage/my/bookmark/page.tsx
+++ frontend/src/app/(with-header)/(mypage)/mypage/my/bookmark/page.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 
 export default async function MyPageBookmarkPage() {
   const bookmarkList = await getMyPageBookmarks();
-  if (!bookmarkList.success) return <>failed</>;
+  if (!bookmarkList.success)
+    return (
+      <>
+        <div className="w-full h-full flex flex-col break-words justify-center items-center text-3xl text-center text-main">
+          북마크를 불러오는데 실패했습니다. <hr /> 다시 시도해주세요.
+        </div>
+      </>
+    );
 
   return (
     <>
