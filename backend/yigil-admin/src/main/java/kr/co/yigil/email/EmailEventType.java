26c1b568dbc3e20b68bfc6bcf81bf92bf975279d
ShineCorine <imsjm81@gmail.com>
2024-03-16 22:59:21 +0900

    관리자 신고 처리 이메일 전송 기능 추가

--- backend/yigil-admin/src/main/java/kr/co/yigil/email/EmailEventType.java
+++ backend/yigil-admin/src/main/java/kr/co/yigil/email/EmailEventType.java
@@ -1,16 +1,20 @@
 package kr.co.yigil.email;
 
+
 public enum EmailEventType {
-    ADMIN_SIGN_UP_ACCEPT {
-        @Override
-        public String toString() {
-            return "accept";
-        }
-    },
-    ADMIN_SIGN_UP_REJECT {
-        @Override
-        public String toString() {
-            return "reject";
-        }
+
+    ADMIN_SIGN_UP_ACCEPT("accept"),
+    ADMIN_SIGN_UP_REJECT("reject"),
+    REPORT_REPLY("report-reply.html");
+
+    private final String templateName;
+
+    EmailEventType(String templateName) {
+        this.templateName = templateName;
+    }
+
+    @Override
+    public String toString() {
+        return templateName;
     }
 }
