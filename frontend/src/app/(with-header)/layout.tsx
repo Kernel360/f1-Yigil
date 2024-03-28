f6f657ef2b809ebc7f6493e3d3caccdcce2e43de
davidktlee <rudxor567@naver.com>
2024-03-29 02:57:09 +0900

    fix: 알림 요청 api url 수정

--- frontend/src/app/(with-header)/layout.tsx
+++ frontend/src/app/(with-header)/layout.tsx
@@ -1,16 +1,21 @@
 import type { ReactNode } from 'react';
 import '../globals.css';
 import Header from '../_components/header/Header';
+import NextPublicProvider from '@/context/NextPublicContext';
 
 export default async function WithHeaderLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const { DEV_BASE_URL, BASE_URL, ENVIRONMENT } = process.env;
+  const currentBaseUrl = ENVIRONMENT === 'production' ? BASE_URL : DEV_BASE_URL;
   return (
-    <section className="w-full h-full flex flex-col">
-      <Header />
-      {children}
-    </section>
+    <NextPublicProvider nextBaseUrl={currentBaseUrl}>
+      <section className="w-full h-full flex flex-col">
+        <Header />
+        {children}
+      </section>
+    </NextPublicProvider>
   );
 }
